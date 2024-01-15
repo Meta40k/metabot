42803b65247fc6e8b9821e4a51ad94d5debfa067
Денис Лопаткин <meta40k@outlook.com>
2024-01-15 23:22:47 +0300

    save

--- Program.cs
+++ Program.cs
@@ -1,7 +1,32 @@
-﻿internal class Program
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using VoiceTexterBot;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    public static async Task Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.OutputEncoding = Encoding.Unicode;
+ 
+        // Объект, отвечающий за постоянный жизненный цикл приложения
+        var host = new HostBuilder()
+            .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+            .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+            .Build(); // Собираем
+ 
+        Console.WriteLine("Сервис запущен");
+        // Запускаем сервис
+        await host.RunAsync();
+        Console.WriteLine("Сервис остановлен");
+    }
+
+    static void ConfigureServices(IServiceCollection services)
+    {
+        // Регистрируем объект TelegramBotClient c токеном подключения
+        services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6776734156:AAFGlUBuMBSU8Cdlf77hJNHm_SpF6nJ7LaM"));
+        // Регистрируем постоянно активный сервис бота
+        services.AddHostedService<Bot>();
     }
 }
