0eaa93c55d90af5bbe57c5d90428c7d9e537cdec
Denis Lopatkin <56197693+Meta40k@users.noreply.github.com>
2024-01-15 19:12:53 +0300

    save

---
+++ Bot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metabot
+{
+    internal class Bot
+    {
+    }
+}
